cee420e9ad08361bbed0304e126822816d612940
Orest Voloshchuk <orest.v@gmail.com>
2011-03-16 22:45:12 +0200

    Added UPGMA CMakeLists lib

--- src/cluster/UPGMA.h
+++ src/cluster/UPGMA.h
@@ -1,7 +1,21 @@
 #ifndef _upgma_h_
 #define _upgma_h_
 
+#include <list>
+#include "Cluster.h"
+
+using namespace std;
+
 class UPGMA {
+    public:
+        UPGMA(DataContainer *pContainer);
+        ~UPGMA();
+
+        void clusterize();
+
+
+    private:
+        list<Cluster*> _clusters;
 
 };
 
