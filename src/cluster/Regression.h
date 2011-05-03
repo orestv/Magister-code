2809e0c6130b9dcc3827d66eab75910cf20a6d2e
Orest Voloshchuk <orest.v@gmail.com>
2011-05-04 02:21:19 +0300

    Merge success

---
+++ src/cluster/Regression.h
@@ -0,0 +1,10 @@
+#ifndef _REGRESSION_H
+#define REGRESSION_H
+
+class Regression {
+    public:
+        static float predict (float *x, float *y, float arg, int nCount);
+
+};
+
+#endif
--- src/main/Regression.cpp
+++ src/main/Regression.cpp
@@ -1,7 +1,24 @@
 #include "Regression.h"
 #include <cmath>
+#include <stdio.h>
 
-float Regression::predict(float **matrix, float *known, float *pred, int nRows, int nCols) {
+float Regression::predict(float *x, float *y, float arg, int nCount) {
+    //y(x) = a + bx
+    float a = 0, b = 0;
+    float n = (float)nCount;
 
-	return 0;
+    float xy = 0, x_ = 0, y_ = 0, x_2 = 0;
+    for (int i = 0; i < n; i++) {
+        xy += x[i]*y[i];
+        x_ += x[i];
+        y_ += y[i];
+        x_2 += pow(x[i], 2);
+     }
+    float top = x_*y_ - n*xy;
+    float bottom = x_*x_ - n*x_2;
+
+    b = top/bottom;
+    a = y_/n - b*x_/n;
+
+    return a + b*arg;
 }
