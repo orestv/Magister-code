c285fd428d1b4d08c46afb3ec4e9460cc190c80e
Orest Voloshchuk <orest.v@gmail.com>
2011-06-09 16:58:32 +0300

    Fixed time stuff

--- src/cluster/dbscan.cpp
+++ src/cluster/dbscan.cpp
@@ -81,7 +81,8 @@ Clustering* DBScan::clusterize(float eps, int nRequiredNeighborCount, AbstractMe
         }
         double fIterationTime = (t_end.tv_sec-t_start.tv_sec) + (double)(t_end.tv_usec-t_start.tv_usec)/1000000;
         gettimeofday(&t_end, 0);
-        printf("%.5f\n", fIterationTime);
+        fprintf(fTime, "%.5f\n", fIterationTime);
+        printf("Iteration time: %.5f\n", fIterationTime);
         printf("\n");
     }
     fclose(fTime);
